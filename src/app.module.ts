46ab4b074638943d805f7e85f8dda82d44657dc2
gabeaugusto <gabeaugusto@fetch.ly>
2023-04-03 23:58:11 -0300

    feat: Creates cats module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/first-project-nest', {
       retryAttempts: 1,
     }),
+    CatsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
